5dea10a593d4abea4e7bebb67c1cbbb510b082bd
Alan Joffre <104494637+alanjoffre@users.noreply.github.com>
2022-12-26 11:32:13 -0300

    second commit

--- aula10/app.py
+++ aula10/app.py
@@ -1,11 +1,11 @@
 # Enviando dados para o template
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def notas():
-    return render_templates('notas.html')
+    return render_template('notas.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
